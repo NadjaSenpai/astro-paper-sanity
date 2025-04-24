1e2c3f524fd0898e81823644e5decb0cdd08ef1c
NadjaSenpai <harsch.noise@gmail.com>
2025-04-24 18:54:39 +0900

    Deploy to Vercel

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "sanity";
-import { structureTool } from 'sanity/structure'
+import { structureTool } from "sanity/structure";
 import { codeInput } from "@sanity/code-input";
 import schemas from "./schemas";
+import { sanityConfig } from "./env";
 
 export default defineConfig({
-  projectId: process.env.SANITY_PROJECT_ID!,
-  dataset: process.env.SANITY_DATASET!,
+  projectId: sanityConfig.projectId,
+  dataset: sanityConfig.dataset,
   title: "My Blog",
   plugins: [structureTool(), codeInput()],
   schema: {
