10db1461adc26875a1ecc334fda1625e30901cf9
NadjaSenpai <harsch.noise@gmail.com>
2025-04-25 01:09:59 +0900

    Deploy Studio

--- studio/env.ts
+++ studio/env.ts
@@ -1,4 +1,4 @@
-export const projectId = import.meta.env.SANITY_PROJECT_ID!;
+export const projectId = import.meta.env.SANITY_PROJECT_ID || "bwyjt9uz";
 export const dataset = import.meta.env.SANITY_DATASET || "production";
 export const apiVersion = import.meta.env.SANITY_API_VERSION || "2025-04-24";
 export const token = import.meta.env.SANITY_API_TOKEN;
