67897097ec5e6653f3658cb88e508d76b8014eec
NadjaSenpai <harsch.noise@gmail.com>
2025-05-01 13:10:48 +0900

    Fix deploy

--- src/pages/og.png.ts
+++ src/pages/og.png.ts
@@ -1,11 +1,20 @@
 import type { APIRoute } from "astro";
 import { generateOgImageForSite } from "@/utils/generateOgImages";
-import { getSiteConfig } from "@/lib/getSiteConfig";
+import { createClient } from "@sanity/client";
 
 export const prerender = true;
 
 export const GET: APIRoute = async () => {
-  const site = await getSiteConfig();
+  const client = createClient({
+    projectId: process.env.SANITY_PROJECT_ID!,
+    dataset: process.env.SANITY_DATASET || "production",
+    apiVersion: "2025-04-21",
+    useCdn: false,
+    token: process.env.SANITY_API_TOKEN,
+  });
+
+  const site = await client.fetch(`*[_type == "settings"][0]`);
+
   const png = await generateOgImageForSite({ site });
 
   return new Response(png, {
