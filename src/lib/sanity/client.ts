31459f967c4ab1d5f7bcc46480446f0fc1addec3
NadjaSenpai <harsch.noise@gmail.com>
2025-05-03 00:30:38 +0900

    Fix: syntax highlighting and embed features

--- src/lib/sanity/client.ts
+++ src/lib/sanity/client.ts
@@ -1,6 +1,6 @@
 // src/lib/sanity/client.ts
-export const projectId = import.meta.env.PUBLIC_SANITY_PROJECT_ID;
-export const dataset   = import.meta.env.PUBLIC_SANITY_DATASET;
-export const apiVersion = "2025-05-02";
-export const useCdn     = true;
-export const token      = import.meta.env.SANITY_API_TOKEN;
+export const projectId   = import.meta.env.SANITY_PROJECT_ID!;
+export const dataset     = import.meta.env.SANITY_DATASET!;
+export const apiVersion  = import.meta.env.SANITY_API_VERSION!;
+export const useCdn      = true;
+export const token       = import.meta.env.SANITY_TOKEN;
