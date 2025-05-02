a5da79352352183be0cb448a9e8581ad09db74ef
NadjaSenpai <harsch.noise@gmail.com>
2025-05-03 00:36:48 +0900

    Fix: 500 error

--- src/lib/sanity/client.ts
+++ src/lib/sanity/client.ts
@@ -1,6 +1,6 @@
 // src/lib/sanity/client.ts
-export const projectId   = import.meta.env.SANITY_PROJECT_ID!;
-export const dataset     = import.meta.env.SANITY_DATASET!;
-export const apiVersion  = import.meta.env.SANITY_API_VERSION!;
+export const projectId   = import.meta.env.SANITY_PROJECT_ID || "bwyjt9uz";
+export const dataset     = import.meta.env.SANITY_DATASET || "production";
+export const apiVersion  = import.meta.env.SANITY_API_VERSION || "2025-05-03";
 export const useCdn      = true;
 export const token       = import.meta.env.SANITY_TOKEN;
